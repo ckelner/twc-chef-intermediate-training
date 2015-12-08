3949650d658011a37aff1ecfc70bd447235a79fc
ckelner <chris.kelner@gmail.com>
2015-12-08 11:42:32 -0500

    Add some new attributes and values for "clowns"

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,3 +1,4 @@
 default["apache"]["indexfile"] = "index1.html"
-default["apache"]["sites"]["clowns"] = { "port" => 80 }
-default["apache"]["sites"]["bears"] = { "port" => 81 }
+default["apache"]["sites"]["clowns"] = { "port" => 80, "nose" => "red" }
+default["apache"]["sites"]["bears"] = { "port" => 81, "nose" => "black" }
+default["apache"]["sites"]["zebras"] = { "port" => 82, "nose" => "striped" }
