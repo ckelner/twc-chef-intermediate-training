35ac3e68ffd486c99d474383032db7c79bc4f916
ckelner <chris.kelner@gmail.com>
2015-12-09 12:14:58 -0500

    write spec for motd & fix it

--- cookbooks/motd/spec/unit/default_spec.rb
+++ cookbooks/motd/spec/unit/default_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe 'motd::default' do
   let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
-  it 'does something' do
-    skip 'need to write this test'
+  it 'creates an motd correctly' do
+    expect(chef_run).to create_template('/etc/motd').with(
+      user: 'root',
+      group: 'root',
+      mode: '0644'
+    )
   end
 end
